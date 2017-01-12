f4790fc0fbd93ee68be78ed22971f397dc750b80
MazinTok <maltokhais@hotmail.com>
2017-01-12 12:45:31 +0300

    Initial commit

---
+++ app/src/main/java/com/mazintokhais/projects/riyadhcalendar/HTMLRemoverBean.java
@@ -0,0 +1,13 @@
+package com.mazintokhais.projects.riyadhcalendar;
+
+/**
+ * Created by mazoo_000 on 14/04/2015.
+ */
+public class HTMLRemoverBean {
+
+    public String title;
+    public String description;
+    public String link;
+    public String pubdate;
+
+}
